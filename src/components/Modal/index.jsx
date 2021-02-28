ae35c810597c8c35eebc1cfcab25f19f96b24bf1
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-02-27 22:33:00 -0300

    Criação do componente Modal

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import "./styles.css";
+
+function Modal({ title, content, options, onClose }) {
+  return (
+    <div className="modal-container">
+      <div className="modal-content">
+        <AiFillCloseCircle size={30} onClick={onClose} />
+        <h2>{title}</h2>
+        <p>{content}</p>
+        <div className="options">{options}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
