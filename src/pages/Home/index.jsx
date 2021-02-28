da71ed4ded351db023c311ade705e1c7dc62ed13
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-02-28 16:45:56 -0300

    Reorganização dos components na pagina

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Appbar from "../../components/Appbar";
 import Card from "../../components/Card";
-import { FaTemperatureHigh, FaArrowDown, FaArrowUp } from "react-icons/fa";
+import {
+  FaTemperatureHigh,
+  FaArrowDown,
+  FaArrowUp,
+  FaCompressArrowsAlt,
+} from "react-icons/fa";
 import { WiHumidity, WiStrongWind } from "react-icons/wi";
+import { TiWaves } from "react-icons/ti";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { useWeather } from "../../contexts/weather";
 import "./styles.css";
 
 function Home() {
-  const { weather } = useWeather();
+  const { weather, count } = useWeather();
 
   return (
     <div className="container">
@@ -16,103 +22,114 @@ function Home() {
       {weather ? (
         <div className="content">
           <div className="grid">
-            <div className="principal-card">
-              <Card
-                style={{
-                  width: "calc(60% + 60px)",
-                  height: "80%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <div className="principal-card-grid">
-                  <div
-                    style={{
-                      gridColumnStart: 1,
-                      gridColumnEnd: 1,
-                    }}
-                  >
-                    <h2 className="name">Clima em {weather.name}</h2>
-                    <img
-                      className="principal-card-icon"
-                      src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-                      alt="Icone do clima"
-                    />
-                    <p>{weather.weather[0].description}</p>
+            <div className="cards">
+              <div className="principal-card">
+                <Card>
+                  <div className="principal-card-grid">
+                    <div
+                      style={{
+                        gridColumnStart: 1,
+                        gridColumnEnd: 1,
+                      }}
+                    >
+                      <h2 className="name">Clima em {weather.name}</h2>
+                      <img
+                        className="principal-card-icon"
+                        src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                        alt="Icone do clima"
+                      />
+                      <p>{weather.weather[0].description}</p>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <WiStrongWind size={30} />
+                      <p style={{ textTransform: "lowercase" }}>
+                        {weather.wind.speed} m/s
+                      </p>
+                    </div>
                   </div>
-                  <div
+                </Card>
+              </div>
+              <div className="adjacent-cards">
+                <div className="adjacent-card">
+                  <Card
                     style={{
-                      display: "flex",
-                      flexDirection: "column",
+                      width: "50%",
+                      maxWidth: 300,
+                      height: "100%",
+                      maxHeight: 300,
+                      display: "grid",
+                      gridTemplateColumns: "1fr 1fr",
+                      gridTemplateRows: "1fr 1fr 1fr",
                       alignItems: "center",
-                      justifyContent: "center",
+                      justifyItems: "center",
+                      textAlign: "center",
                     }}
                   >
-                    <WiStrongWind />
+                    <FaTemperatureHigh
+                      size={27}
+                      color="#E4CF0E"
+                      className="icon"
+                    />
+                    <p>{weather.main.temp}ºC</p>
+                    <FaArrowDown size={30} color="#0E24E4" className="icon" />
+                    <p>{weather.main.temp_min}ºC</p>
+                    <FaArrowUp size={30} color="#E40E0E" className="icon" />
+                    <p>{weather.main.temp_max}ºC</p>
+                  </Card>
+                </div>
+                <div className="adjacent-card">
+                  <Card
+                    style={{
+                      width: "50%",
+                      maxWidth: 300,
+                      height: "100%",
+                      maxHeight: 300,
+                      display: "grid",
+                      gridTemplateColumns: "1fr 1fr",
+                      gridTemplateRows: "1fr 1fr 1fr",
+                      alignItems: "center",
+                      justifyItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <TiWaves size={40} color="#E4CF0E" className="icon" />
                     <p style={{ textTransform: "lowercase" }}>
-                      {weather.wind.speed} m/s
+                      {weather.main.feels_like}ºC
                     </p>
-                  </div>
+                    <WiHumidity size={40} color="#0E24E4" className="icon" />
+                    <p>{weather.main.humidity}%</p>
+                    <FaCompressArrowsAlt
+                      size={25}
+                      color="#E40E0E"
+                      className="icon"
+                    />
+                    <p style={{ textTransform: "lowercase" }}>
+                      {weather.main.pressure} atm
+                    </p>
+                  </Card>
                 </div>
-              </Card>
-            </div>
-            <div className="adjacent-cards">
-              <Card
-                style={{
-                  width: "30%",
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <div className="adjacent-cards-icons">
-                  <FaTemperatureHigh size={30} className="icon" />
-                  <p>{weather.main.temp}ºC</p>
-                </div>
-                <div className="adjacent-cards-icons">
-                  <FaArrowDown size={30} className="icon" />
-                  <p>{weather.main.temp_min}ºC</p>
-                </div>
-                <div className="adjacent-cards-icons">
-                  <FaArrowUp size={30} className="icon" />
-                  <p>{weather.main.temp_max}ºC</p>
-                </div>
-              </Card>
-              <Card
-                style={{
-                  width: "30%",
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <div className="adjacent-cards-icons">
-                  <WiHumidity size={30} className="icon" />
-                  <p>{weather.main.humidity}%</p>
-                </div>
-                <p>Sensção termica: {weather.main.feels_like}ºC</p>
-                <p>Pressão: {weather.main.pressure} atm</p>
-              </Card>
+              </div>
             </div>
             <div className="map">
               <MapContainer
                 key={count}
-              style={{
+                style={{
                   height: "100%",
                   width: "100%",
                   maxWidth: 1000,
                   margin: "auto auto",
-              }}
+                }}
                 center={[weather.coord.lat, weather.coord.lon]}
                 zoom={10}
                 scrollWheelZoom={true}
-            >
+              >
                 <TileLayer
                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
